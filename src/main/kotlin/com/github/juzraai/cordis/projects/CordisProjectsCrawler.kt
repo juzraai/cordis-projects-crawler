ea40289fccf83a19f0bcd6a765ad2ccf2226bbdd
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2018-01-28 15:30:53 +0100

    Hello World

---
+++ src/main/kotlin/com/github/juzraai/cordis/projects/CordisProjectsCrawler.kt
@@ -0,0 +1,15 @@
+package com.github.juzraai.cordis.projects
+
+/**
+ * @author Zsolt Jurányi
+ */
+fun main(args: Array<String>) {
+	CordisProjectsCrawler().start(args)
+}
+
+class CordisProjectsCrawler {
+
+	fun start(args: Array<String>) {
+		println("Hello World!")
+	}
+}
