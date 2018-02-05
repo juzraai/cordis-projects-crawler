a554411a4d9b8690d05e2b6008758786909192cc
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2018-02-05 17:54:46 +0100

    Note on fetching all RCNs

--- src/main/kotlin/com/github/juzraai/cordis/crawler/seed/AllRcnsSeed.kt
+++ src/main/kotlin/com/github/juzraai/cordis/crawler/seed/AllRcnsSeed.kt
@@ -9,6 +9,9 @@ class AllRcnsSeed : IRcnSeedGenerator {
 
 	// TODO later: able to fetch highest result RCN
 
+	// TODO fetch all RCNs from a CORDIS query URL given by user!!!
+	// TODO always fetch PROJECTS only, and their related results?
+
 	override fun generateRcns(scope: String?, configuration: CordisCrawlerConfiguration): Sequence<Long>? {
 		if ("all".equals(scope, true)) {
 			// TODO fetch highest project RCN from CORDIS
