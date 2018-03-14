781b4112d8d2129dda9e4fd68c3112650a4974c8
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2018-03-15 00:01:52 +0100

    MySQL exporter - some relations

--- src/main/kotlin/com/github/juzraai/cordis/crawler/modules/exporters/CordisProjectMysqlExporter.kt
+++ src/main/kotlin/com/github/juzraai/cordis/crawler/modules/exporters/CordisProjectMysqlExporter.kt
@@ -2,88 +2,78 @@ package com.github.juzraai.cordis.crawler.modules.exporters
 
 import com.github.juzraai.cordis.crawler.model.*
 import com.github.juzraai.cordis.crawler.model.cordis.*
-import org.apache.commons.dbcp2.*
-import java.sql.*
+import com.github.juzraai.cordis.crawler.util.*
+import mu.*
 import java.util.*
-import javax.sql.*
 
 /**
  * @author Zsolt Jurányi
  */
 class CordisProjectMysqlExporter : ICordisProjectExporter {
 
-	var dataSource: DataSource? = null
+	companion object : KLogging()
+
+	var db: Database? = null
 
 	override fun initialize(configuration: CordisCrawlerConfiguration) {
 		// TODO read config
-		dataSource = BasicDataSource().apply {
-			url = "jdbc:mysql://localhost/cordis?useUnicode=true&characterEncoding=utf8&useSSL=false"
-			username = "root"
-			password = "root"
-		}
+		// TODO enable/disable?
+		db = Database("localhost", 3306, "cordis", "root", "root")
 
 		javaClass.classLoader.getResourceAsStream("mysql_create_tables.sql").use { stream ->
 			Scanner(stream).useDelimiter("\\A").use { scanner ->
-				executeScript(scanner.next())
+				db?.executeScript(scanner.next())
 			}
 		}
 	}
 
 	override fun exportCordisProjects(cordisProjects: List<CordisProject>) {
-		useConnection { connection ->
-			val projectArrays = cordisProjects.mapNotNull(CordisProject::project).map(this::projectToArray)
-			batchReplace(connection, "cordis_project", projectArrays)
-			// TODO valahogy össze kéne gyújteni a tárolandó objektumokat táblák szerint listába
-			// TODO relations-t kitalálni
-		}
+		// TODO enable/disable
+
+		exportProjects(cordisProjects.mapNotNull(CordisProject::project))
+		// TODO if -p, exportPublications
+
 	}
 
-	private fun batchReplace(connection: Connection, table: String, arrays: List<Array<Any?>>) {
-		if (arrays.isEmpty()) return
-		val questionMarks = arrays[0].joinToString(",") { "?" }
-		val sql = "replace into $table values ($questionMarks)"
-		val ps = connection.prepareStatement(sql)
-		try {
-			arrays.forEach { array ->
-				array.forEachIndexed { i, value ->
-					ps.setObject(i + 1, value)
-				}
-				ps.addBatch()
+	private fun exportProjects(projects: List<Project>) {
+		db?.batchReplace("cordis_project", projects.map(this::projectToArray))
+		projects.onEach {
+			if (null != it.rcn && null != it.relations) {
+				exportRelations(it.rcn!!, "project", it.relations!!)
 			}
-			ps.executeBatch()
-		} catch (e: Exception) {
-			e.printStackTrace()
-			// TODO log
-		} finally {
-			ps.close()
 		}
 	}
 
-	private fun executeUpdate(connection: Connection, sql: String): Int {
-		val ps = connection.prepareStatement(sql)
-		var r = -1
-		try {
-			r = ps.executeUpdate()
-		} catch (e: Exception) {
-			e.printStackTrace()
-			// TODO log
-		} finally {
-			ps.close()
-		}
-		return r
-	}
+	private fun exportRelations(ownerId: Long, ownerType: String, relations: Relations) {
+		val relationArrays = mutableListOf<Array<Any?>>()
 
-	private fun executeScript(sql: String) {
-		useConnection { connection ->
-			// this splitting is not elegant, I know,
-			// but it's fair enough in our case
-			sql.split(";")
-					.map(String::trim)
-					.filter { it.isNotBlank() }
-					.forEach { statement ->
-						executeUpdate(connection, statement)
-					}
+		// TODO kéne valami generikusabb megoldás a relation előállításra
+		// Any-ből kiszedhetné az ID-t: rcn, code mező; vagy toString SHA-1 hash-e
+		// a type-ot: typeAttr, vagy ha nincs olyan mező, akkor type
+		// és a többit
+
+		val categoryArrays = mutableListOf<Array<Any?>>()
+		relations.categories?.filter { null != it.code }?.forEach { category ->
+			//categoryArrays.add(categoryToArray(category))
+			relationArrays.add(relationToArray(
+					ownerId, ownerType,
+					category.code!!, "category",
+					category.type, category.classification
+			))
 		}
+		db?.batchReplace("cordis_category", categoryArrays)
+
+		val regionArrays = mutableListOf<Array<Any?>>()
+		relations.regions?.filter { null != it.rcn }?.forEach { region ->
+			//regionArrays.add(regionToArray(region))
+			relationArrays.add(relationToArray(
+					ownerId, ownerType,
+					region.rcn!!.toString(), "region",
+					region.type))
+		}
+		db?.batchReplace("cordis_region", regionArrays)
+
+		db?.batchReplace("cordis_relation", relationArrays)
 	}
 
 	private fun projectToArray(project: Project): Array<Any?> {
@@ -114,15 +104,24 @@ class CordisProjectMysqlExporter : ICordisProjectExporter {
 		}
 	}
 
-	private fun useConnection(block: (Connection) -> Unit) {
-		val connection = dataSource?.connection
-		try {
-			block.invoke(connection!!)
-		} catch (e: Exception) {
-			e.printStackTrace()
-			// TODO log
-		} finally {
-			connection?.close()
-		}
+	private fun relationToArray(
+			ownerId: Long, ownerType: String, ownedId: String, ownedType: String, type: String?,
+			classification: String? = null, context: Boolean? = null, ecContribution: Double? = null,
+			order: Int? = null, terminated: Boolean? = null): Array<Any?> {
+		return arrayOf(
+				"$ownerType/$ownerId-$type-$ownedType/$ownedId",
+				ownerId,
+				ownerType,
+				ownedId,
+				ownedType,
+				type,
+				classification,
+				context,
+				ecContribution,
+				order,
+				terminated
+		)
 	}
+
+
 }
---
+++ src/main/kotlin/com/github/juzraai/cordis/crawler/util/Database.kt
@@ -0,0 +1,87 @@
+package com.github.juzraai.cordis.crawler.util
+
+import com.github.juzraai.cordis.crawler.modules.exporters.*
+import mu.*
+import org.apache.commons.dbcp2.*
+import java.sql.*
+import javax.sql.*
+
+/**
+ * @author Zsolt Jurányi
+ */
+class Database(host: String, port: Int, schema: String, username: String, password: String) {
+
+	companion object : KLogging()
+
+	var dataSource: DataSource = BasicDataSource().apply {
+		url = "jdbc:mysql://$host:$port/$schema?useUnicode=true&characterEncoding=utf8&useSSL=false"
+		this.username = username
+		this.password = password
+	}
+
+	fun batchReplace(table: String, arrays: List<Array<Any?>>) {
+		if (arrays.isEmpty()) return
+		val questionMarks = arrays[0].joinToString(",") { "?" }
+		val sql = "replace into $table values ($questionMarks)"
+		useConnection { connection ->
+			val ps = connection.prepareStatement(sql)
+			try {
+				arrays.forEach { array ->
+					array.forEachIndexed { i, value ->
+						ps.setObject(i + 1, value)
+					}
+					ps.addBatch()
+				}
+				logger.trace("Inserting ${arrays.size} records into `$table`")
+				ps.executeBatch()
+			} catch (e: Exception) {
+				logger.error("Failed to insert records into `$table`", e)
+			} finally {
+				ps.close()
+			}
+		}
+	}
+
+	fun executeUpdate(sql: String): Int {
+		var r = 0
+		useConnection { r = executeUpdate(it, sql) }
+		return r
+	}
+
+	private fun executeUpdate(connection: Connection, sql: String): Int {
+		val ps = connection.prepareStatement(sql)
+		var r = -1
+		try {
+			r = ps.executeUpdate()
+		} catch (e: Exception) {
+			CordisProjectMysqlExporter.logger.error("Failed to run SQL command", e)
+		} finally {
+			ps.close()
+		}
+		return r
+	}
+
+	fun executeScript(sql: String) {
+		useConnection { connection ->
+			// this splitting is not elegant, I know,
+			// but it's fair enough in our case
+			sql.split(";")
+					.map(String::trim)
+					.filter { it.isNotBlank() }
+					.forEach { statement ->
+						executeUpdate(connection, statement)
+					}
+		}
+	}
+
+	private fun useConnection(block: (Connection) -> Unit) {
+		val connection = dataSource?.connection
+		try {
+			block.invoke(connection!!)
+		} catch (e: Exception) {
+			logger.error("Failed to perform operations with connection", e)
+		} finally {
+			connection?.close()
+		}
+	}
+}
